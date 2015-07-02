73019dee3d7634db1f6277a671ebd88665f40be7
MarcDuboc <marc.duboc@cifacom.com>
2015-07-02 09:53:04 +0200

    YOLOOO

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 var angular = require('angular');
 
+API_KEY = 'bcf6ff211b659eb30e136e7df58d2bca';
+
 var apiReader = angular.module('apiReader', [require('angular-ui-router')]);
 
 apiReader.config(require('./routing.js'));
 
 apiReader.controller('HomeController', require('./controller/HomeController.js'));
+apiReader.controller('DetailsController', require('./controller/DetailsController.js'));
